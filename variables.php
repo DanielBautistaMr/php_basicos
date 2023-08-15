9e73a24d008fc61d03c8fc5535cae73bb2399aee
Laboratorio de Telecomunicaciones <cisco@academico.unab.ad>
2023-08-15 18:27:18 -0500

    el segundo commit

---
+++ variables.php
@@ -0,0 +1,22 @@
+<?php
+// variables
+$nombre = "Jorge";
+$edad = 22;
+$double = 22.3;
+$boolean = true;
+$nombreCurso = "Backend";
+
+
+//concatenacion
+
+$concatenacion = $nombre."<br>".$edad."<br>".$double;
+
+echo $concatenacion;
+
+
+
+
+
+
+?>
+
